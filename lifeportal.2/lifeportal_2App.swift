4f5e352e5ef27023c40c9fdd730b6d7b548f89c3
VMescape <goldpatrick@hotmail.com>
2025-04-19 20:23:19 +1000

    Initial Commit

---
+++ lifeportal.2/lifeportal_2App.swift
@@ -0,0 +1,17 @@
+//
+//  lifeportal_2App.swift
+//  lifeportal.2
+//
+//  Created by pat on 19/4/2025.
+//
+
+import SwiftUI
+
+@main
+struct lifeportal_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ lifeportal.2Tests/lifeportal_2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  lifeportal_2Tests.swift
+//  lifeportal.2Tests
+//
+//  Created by pat on 19/4/2025.
+//
+
+import Testing
+@testable import lifeportal_2
+
+struct lifeportal_2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
